7d9c82cfa33db9b2ba750456e29f0f641e70bac0
nicolas perier <nicolas_perier@mailo.com>
2022-05-19 10:07:27 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+LoveHouse = { name: 'Love', address: '7 Boundary St, London E2 7JE', description: 'perfect for start making children, all the house is thinking for love', price_per_night: 20 }
+SportHouse = { name: 'Sport', address: '56A Shoreditch High St, London E1 6PQ', description: 'No chairs, just yoour legs if you want to chill, musculation is the key opf your life', price_per_night: 54 }
+HolidaysHouse = { name: 'Holiday', address: '59 Chill avenue, Lazy 57 990 A', description: 'Belle maison pour les gros sacs qui ont juste envie de lézarder toute la journée', price_per_night: 120 }
+FamilyHouse =  { name: 'Family', address: '3 Horreur rue, Torture B1 6ZQ', description: 'Pour de belle vacances en famille, une super sale de torture et de séquestration pour vos enfants. Les vacances sont toujours plus belle avec les enfants', price_per_night: 350 }
+
+[ LoveHouse, SportHouse, HolidaysHouse, FamilyHouse ].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
